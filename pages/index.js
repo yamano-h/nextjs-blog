ac3bf334546ce038cdf5d5f61e3ac1a7b44a2ab7
yamano-h <yamano.hidenori@gmail.com>
2020-06-20 20:28:08 +0900

    change title

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,9 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>…</Head>
+      <Head>
+        <title>Top Page</title>
+      </Head>
       <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
